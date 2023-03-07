da4a00de51b5d6443891c038f6ed9c17c598d020
nlprasanna5 <lakshmi.76740@gmail.com>
2023-03-07 22:19:43 +0530

    created registered form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="register">
+      <h1>Register here ...</h1>
+      <input type="text" placeholder='mobile ...' />
+      <input type="text" placeholder="password ..." />
+      <button>Submit</button>
     </div>
+
   );
 }
 
